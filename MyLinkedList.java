8e260de43b1986927937f5c7903bd87618d5bb27
dramsammy <dramsammy@stuy.edu>
2019-04-01 10:45:32 -0400

    added mylinkedlist

---
+++ MyLinkedList.java
@@ -0,0 +1,15 @@
+public class MyLinkedList{
+    public String toString()
+
+    public MyLinkedList()
+
+    public void clear()
+
+    public boolean/void add(E)
+
+    public void extend(MyLinkedList<E> other)
+
+    public E removeFront()
+
+  }
+  
