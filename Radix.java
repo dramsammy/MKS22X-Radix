a25cb82488433c3c639791375c4ce79c12802fff
dramsammy <dramsammy@stuy.edu>
2019-04-02 11:01:26 -0400

    working on radix sort

--- Radix.java
+++ Radix.java
@@ -1,5 +1,17 @@
 public class Radix{
   public static void radixsort(int[]data){
-    MyLinkedList list = new MyLinkedList
+    int largest = 0;
+    MyLinkedList<Integer>[] buckets = new MyLinkedList[20];
+    for (int i = 0; i < 20; i++){
+      String x = (Integer.toString(i));
+      MyLinkedList<Integer>[] x = new MyLinkedList[0];
+    }
+    for (int i = 0; i < data.length; i++){
+      if (data[i] > largest){
+        largest = data[i];
+      }
+    }
+    Integer.toString(largest).subString(0,1).add(largest);
+    }
+
   }
-}
