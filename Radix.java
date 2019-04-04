746b2621fcd07df3494cf67a4ef6c4b8f4d1eefd
dramsammy <dramsammy@stuy.edu>
2019-04-04 01:05:58 -0400

    fixed radix and tested

--- Radix.java
+++ Radix.java
@@ -5,10 +5,10 @@ public class Radix{
     MyLinkedList set = new MyLinkedList();
     for (int i = 0; i < 20; i++){
       if (buckets[i] == null){
-        buckets[i] = new MyLinkedList[0];
+        buckets[i] = new MyLinkedList<Integer>();
     }
   }
-    int num = 0;
+    int k = 0;
     // parsing through to figure out if positive or negative value goes in the bucket
     for (int i = 0; i < data.length; i++){
       if (Math.abs(data[i]) > Math.pow(10, k)) {
@@ -19,27 +19,37 @@ public class Radix{
       // adding it to bucket
       set.add(data[i]);
     }
+    MyLinkedList temp = new MyLinkedList();
     for (int i = 0; i < k; i++) {
-     for (int j = 0; j < data.length; j++) {
+     for (int a = 0; a < data.length; a++) {
        //getting the placevalue
-       int digit = Math.abs(getDigit(data[j], i));
-       if (data[j] > 0)
+       int digit = Math.abs(getDigit(data[a], i));
+       if (data[a] > 0)
        // using buckets in correct order
-         buckets[digit+10].add(data[j]);
+         buckets[digit+10].add(data[a]);
        else {
          // added for negatives
-         buckets[9-digit].add(data[j]);
+         buckets[9-digit].add(data[a]);
      }
    }
+
+   for (int x = 0; x < buckets.length; x++) {
+     //extending buckets to original list
+     if (buckets[x].size() > 0)
+       temp.extend(buckets[x]);
+     }
+   for (int y = 0; y < data.length; y++) {
+     // sorting the data in buckets
+     data[y] = (int)temp.removeFront();
+   }
+ }
+}
 
 
   //function to parse through placevalue for the buckets
   private static int getDigit(int data, int placeValue) {
 		int pos = data;
     pos = pos / (int) Math.pow(10, placeValue) % 10;
-		if (data < 0) {
-			return pos = pos * -1;
-		}
 		return pos;
   }
 }
